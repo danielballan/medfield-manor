4046e10a84a5084296b376b1863c16f384bd30ca
Dan Allan <daniel.b.allan@gmail.com>
2018-09-01 20:49:38 +0000

    A working caproto IOC with safe GPIO shutdown

--- rgb.py
+++ rgb.py
@@ -1,50 +1,77 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+"""
+This example requires a barcode reader and the Python library evdev.
+"""
+import ast
+from caproto.server import pvproperty, PVGroup, template_arg_parser, run
 import RPi.GPIO as GPIO
-import time
 
-colors = [0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0xFF00FF, 0x00FFFF]
-pins = {'pin_R':11, 'pin_G':15, 'pin_B':22}  # pins is a dict
 
-GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-for i in pins:
-    GPIO.setup(pins[i], GPIO.OUT)   # Set pins' mode is output
-    GPIO.output(pins[i], GPIO.LOW) # Set pins to high(+3.3V) to off led
-
-p_R = GPIO.PWM(pins['pin_R'], 2000)  # set Frequece to 2KHz
-p_G = GPIO.PWM(pins['pin_G'], 2000)
-p_B = GPIO.PWM(pins['pin_B'], 5000)
-
-p_R.start(0)      # Initial duty Cycle = 0(leds off)
-p_G.start(0)
-p_B.start(0)
-
-def map(x, in_min, in_max, out_min, out_max):
+def rescale(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-def setColor(col):   # For example : col = 0x112233
-    R_val = (col & 0xff0000) >> 16
-    G_val = (col & 0x00ff00) >> 8
-    B_val = (col & 0x0000ff) >> 0
-    
-    R_val = map(R_val, 0, 255, 0, 100)
-    G_val = map(G_val, 0, 255, 0, 100)
-    B_val = map(B_val, 0, 255, 0, 100)
-    
-    p_R.ChangeDutyCycle(R_val)     # Change duty cycle
-    p_G.ChangeDutyCycle(G_val)
-    p_B.ChangeDutyCycle(B_val)
 
-try:
-    while True:
-        for col in colors:
-            setColor(col)
-            time.sleep(0.5)
-except KeyboardInterrupt:
-    p_R.stop()
-    p_G.stop()
-    p_B.stop()
-    for i in pins:
-        GPIO.output(pins[i], GPIO.LOW)    # Turn off all leds
-        print('killed')
-    GPIO.cleanup()
+def duty_cycle(color):
+    """Given a HEX color, Compute PWM duty cycle percentage each channel."""
+    rgb255 = {'red': (color & 0xff0000) >> 16,
+              'green': (color & 0x00ff00) >> 8,
+              'blue': (color & 0x0000ff) >> 0}
 
+    return {channel: rescale(val, 0, 255, 0, 100)
+            for channel, val in rgb255.items()}
+
+
+class IOC(PVGroup):
+    def __init__(self, *, red, green, blue, **kwargs):
+        super().__init__(**kwargs)
+        self.pins = {'red': int(red), 'green': int(green), 'blue': int(blue)}
+        self.pwm = {}
+
+    async def write_color(self, instance, value):
+        color_base10 = ast.literal_eval(f'0x{value[0]}')
+        for channel, val in duty_cycle(color_base10).items():
+            self.pwm[channel].ChangeDutyCycle(val)
+        return value
+
+    color = pvproperty(value=['000000'], put=write_color)
+
+    @color.startup
+    async def color(self, instance, async_lib):
+        GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+        for pin in self.pins.values():
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, GPIO.LOW)  # LOW -> off
+
+        for channel, pin in self.pins.items():
+            self.pwm[channel] = GPIO.PWM(pin, 2000)  # set frequency to 2KHz
+
+        # Initial duty Cycle = 0(leds off)
+        for pwm in self.pwm.values():
+            pwm.start(0)
+
+    @color.shutdown
+    async def color(self, instance, async_lib):
+        for pwm in self.pwm.values():
+            pwm.ChangeDutyCycle(0)
+        for pwm in self.pwm.values():
+            pwm.stop(0)
+        for pin in self.pins.values():
+            GPIO.output(pin, GPIO.LOW)    # Turn off all leds
+        GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    parser, split_args = template_arg_parser(
+        default_prefix='rpi:',
+        desc='Run an IOC that colors RGB LEDs.',
+        supported_async_libs=('asyncio',))
+
+    for channel in ('red', 'green', 'blue'):
+        parser.add_argument(f'--{channel}',
+                            help=f'The BOARD pin for the {channel} channel',
+                            required=True, type=str)
+
+    args = parser.parse_args()
+    ioc_options, run_options = split_args(args)
+    ioc = IOC(red=args.red, green=args.green, blue=args.blue, **ioc_options)
+    run(ioc.pvdb, **run_options)
